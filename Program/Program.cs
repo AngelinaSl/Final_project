ebd7c929412a6086080b2955d3d5937d2ee2ba6f
Angelina <chulakovaav@yandex.ru>
2022-11-09 13:17:12 +0300

    add array fill method

---
+++ Program/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] FillStringArray(int arrayLength)
+{
+    string[] stringArray = new string[arrayLength];
+    Console.WriteLine("Введите рандомные слово(а) или символы в массив: ");
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        stringArray[i] = Console.ReadLine();
+    }
+    return stringArray;
+}
